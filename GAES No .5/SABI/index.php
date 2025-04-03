2dffb9e40a15614a75463707284e9eb67570ae19
cubeblack22 <rojasmena1222@gmail.com>
2025-04-03 17:47:28 -0500

    Docs: eliminacion de la carpeta de prueba

--- GAES No .5/SABI/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-echo 'hola mundo'
-echo 'Hola familia'
-echo 'nonoonononoo'
-echo 'cuando se te moja la tarea'
-echo 'There was an error committing your changes: cubeblack22 has committed since you started editing.'
-?>
