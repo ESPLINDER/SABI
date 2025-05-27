897d7c6705df5b0746ea1fd830124195e2016f11
cubeblack22 <rojasmena1222@gmail.com>
2025-05-27 08:27:45 -0500

    docs: usuario

--- tiendaDonCarlosNetbeans/src/main/java/Modelo/Usuario.java
+++ tiendaDonCarlosNetbeans/src/main/java/Modelo/Usuario.java
@@ -1,13 +1,75 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Modelo;
-
 /**
- *
- * @author adminsena
+ * @author william el pro
  */
-public class Usuario {
-    
+class Usuario {
+    private int idUsuario;
+    private String emaUsuario;
+    private String passUsuario;
+    private String nomUsuario;
+    private String apeUsuario;
+    private String[] rolUsuario = {"Administrador", "Empleado"};
+
+    public Usuario() {
+    }
+    public Usuario(int idUsuario, String emaUsuario, String passUsuario, String nomUsuario, String apeUsuario) {
+        this.idUsuario = idUsuario;
+        this.emaUsuario = emaUsuario;
+        this.passUsuario = passUsuario;
+        this.nomUsuario = nomUsuario;
+        this.apeUsuario = apeUsuario;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public String getEmaUsuario() {
+        return emaUsuario;
+    }
+
+    public String getPassUsuario() {
+        return passUsuario;
+    }
+
+    public String getNomUsuario() {
+        return nomUsuario;
+    }
+
+    public String getApeUsuario() {
+        return apeUsuario;
+    }
+
+    public String[] getRolUsuario() {
+        return rolUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public void setEmaUsuario(String emaUsuario) {
+        this.emaUsuario = emaUsuario;
+    }
+
+    public void setPassUsuario(String passUsuario) {
+        this.passUsuario = passUsuario;
+    }
+
+    public void setNomUsuario(String nomUsuario) {
+        this.nomUsuario = nomUsuario;
+    }
+
+    public void setApeUsuario(String apeUsuario) {
+        this.apeUsuario = apeUsuario;
+    }
+
+    public void setRolUsuario(String[] rolUsuario) {
+        this.rolUsuario = rolUsuario;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" + "idUsuario=" + idUsuario + ", emaUsuario=" + emaUsuario + ", passUsuario=" + passUsuario + ", nomUsuario=" + nomUsuario + ", apeUsuario=" + apeUsuario + ", rolUsuario=" + rolUsuario + '}';
+    }
 }
