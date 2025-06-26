118934c440e896bf9b783470683a88a0865d621c
cubeblack22 <rojasmena1222@gmail.com>
2025-06-26 07:25:35 -0500

    docs

--- apacheSABI/src/main/java/Modelo/RutinaDao.java
+++ apacheSABI/src/main/java/Modelo/RutinaDao.java
@@ -15,21 +15,4 @@ public class RutinaDao {
     PreparedStatement ps;
     ResultSet rs;
     int r;
-
-    public List<Rutina> listar() {
-            conn = cn.Conexion();
-            ps = conn.prepareStatement(sql);
-            ps.setInt(1, idUsuario);
-            r = ps.executeUpdate();
-            
-            if (r > 0) {
-                System.out.println("Usuario eliminado correctamente");
-            } else {
-                System.out.println("No se encontró el usuario con ID: " + idUsuario);
-            }
-        } catch (ClassNotFoundException | SQLException e) {
-            System.out.println("Error al eliminar usuario: " + e.getMessage());
-        }
-        return r;
-    }
 }
