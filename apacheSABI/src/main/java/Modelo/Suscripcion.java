79147115bc66317ed514bde6ad49a80676907a9b
cubeblack22 <rojasmena1222@gmail.com>
2025-06-28 19:39:19 -0500

    add: creacion de la clase suscripcion

---
+++ apacheSABI/src/main/java/Modelo/Suscripcion.java
@@ -0,0 +1,13 @@
+package Modelo;
+
+/**
+ *
+ * @author Rojas
+ */
+public class Suscripcion {
+    
+    private int idSuscripcion;
+    private int fkIdEntrenador;
+    private int fkIdCliente;
+    private String 
+}
